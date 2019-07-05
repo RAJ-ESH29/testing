a15760f39b9523390ba6afb554bb330e8bf753ee
rajesh.kumar.singh@oracle.com <rkumarsi@sca00hcc.us.oracle.com>
2019-07-04 23:24:14 -0700

    econd commit

--- problem_solver.rb
+++ problem_solver.rb
@@ -1,3 +1,11 @@
-1. class ProblemSolver
-2. Some command
-3. end
+
+ 1 class ProblemSolver
+ 2    def solve_easy_problem
+ 3    end
+ 4
+ 5    def solve_harder_problem
+ 6    end
+ 7  
+ 8    def solve_tough_problem
+ 9    end
+ 10 end
